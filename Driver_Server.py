48a254ce386ec6239cbce6106d11f0e5cf3791a5
Seo Myeong Ho <86174016+OldHama@users.noreply.github.com>
2023-12-18 03:27:39 +0900

    Update Driver_Server.py

--- Driver_Server.py
+++ Driver_Server.py
@@ -2,6 +2,26 @@ from flask import Flask
 import socket
 import RPi.GPIO as GPIO
 import time
+import wave
+import pyaudio
+
+# 오디오 파일 재생 함수
+def play_audio(file_path):
+    wf = wave.open(file_path, 'rb')
+    p = pyaudio.PyAudio()
+    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                    channels=wf.getnchannels(),
+                    rate=wf.getframerate(),
+                    output=True)
+    
+    data = wf.readframes(1024)
+    while len(data) > 0:
+        stream.write(data)
+        data = wf.readframes(1024)
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
 
 GPIO.setwarnings(False)
 app = Flask(__name__)
@@ -14,7 +34,7 @@ def hello():
 def wakeup():
     global handler
     GPIO.output(motor1, True)
-    time.sleep(10)
+    play_audio("/path/to/your/audiofile.wav")
     GPIO.output(motor1, False)
     return "wakeup"
     
